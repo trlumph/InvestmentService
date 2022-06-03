7ae0b6da7c796f196b1fb119c0353addfc4254b5
T1M0UT <gohim322@gmail.com>
2022-06-03 19:21:05 +0300

    Testing example upd

--- InvestmentCalculator/InvestmentCalculator/Program.cs
+++ InvestmentCalculator/InvestmentCalculator/Program.cs
@@ -6,13 +6,15 @@ class Program
 {
     public static void Main()
     {
-        var result = new Investment(
+        var investment = new Investment(
             new DateTime(2022, 06, 1),
             new DateTime(2022, 06, 1),
             200_000,
             0.04m,
             30
-        ).CalculateSumOfFutureInterests();
+        );
+
+        var result = investment.CalculateSumOfFutureInterests();
 
         Console.WriteLine($"${result:N2}");
     }
